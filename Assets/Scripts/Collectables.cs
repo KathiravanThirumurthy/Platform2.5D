56f2650156fad86c84ee19d7f633636984c4b48e
Kathiravan <tkathiravan1976@gmail.com>
2021-04-30 06:43:28 +0530

    Player movement Completed

---
+++ Assets/Scripts/Collectables.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectables : MonoBehaviour
+{
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="Player")
+        {
+            Movemnetscript player = other.GetComponent<Movemnetscript>();
+            if(player!=null)
+            {
+                player.addCoins();
+            }
+            Destroy(this.gameObject);
+           
+        }
+    }
+}
---
+++ Assets/Scripts/Movemnetscript.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movemnetscript : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float gravity;
+    [SerializeField]
+    private float jumpHeight;
+    [SerializeField]
+    private float _yVelocity;
+    private Vector3 directions;
+    private Vector3 velocity;
+    private CharacterController characterController;
+    private bool _candoubleJump=false;
+    private int _coins;
+    private Uimanager _uimanager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _uimanager = GameObject.Find("Canvas").GetComponent<Uimanager>();
+        if (_uimanager == null)
+        {
+            Debug.LogError("Uimanager is null");
+        }
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        directions = new Vector3(h,0,0);
+        velocity = directions * speed;
+        
+        if(characterController.isGrounded == true)
+        {
+            
+           
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _yVelocity = jumpHeight;
+                _candoubleJump = true;
+
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if(_candoubleJump == true)
+                {
+                    _yVelocity += jumpHeight*2;
+                    _candoubleJump = false;
+                }
+               
+
+            }
+            
+            _yVelocity -= gravity;
+        }
+        velocity.y = _yVelocity;
+        characterController.Move(velocity*Time.deltaTime);
+        
+    }
+    public void addCoins()
+    {
+        _coins++;
+        _uimanager.updateScore(_coins);
+       
+    }
+}
